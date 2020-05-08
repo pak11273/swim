bd2e95a9a328c3d2dc39d58a0e1532358e5d5df4
pak11273 <pak11273@gmail.com>
2020-05-08 17:24:43 -0500

    adjustes text on index

--- packages/web/components/Text/index.js
+++ packages/web/components/Text/index.js
@@ -4,9 +4,8 @@ export const Text = styled.p`
   background: transparent;
   color: ${(props) => props.color || props.theme.colors.primary};
   margin: ${(props) => props.margin};
-  padding: ${(props) => props.padding || "20px"};
+  padding: ${(props) => props.padding};
   line-height: ${(props) => props.lineHeight};
-  text-align: ${(props) => props.textAlign || "center"};
   font-size: ${(props) => props.fontSize};
   max-width: ${(props) => props.maxWidth};
 `
